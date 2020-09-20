5edb196163e38eb2c52e6e5db5c7819a404d41cc
Lisa Pena <lisapena13@gmail.com>
2020-09-20 15:44:37 -0500

    Updates

---
+++ precache-manifest.3d38dabdc57df0296fb17054731d0948.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b58b9bbf64c02890b929053e3bf1940f",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "8648ba133720f9675620",
+    "url": "/Tic-Tac-Toe/static/css/main.894ca4fc.chunk.css"
+  },
+  {
+    "revision": "5fa3ff0b5b25f966a348",
+    "url": "/Tic-Tac-Toe/static/js/2.2c2a61e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe/static/js/2.2c2a61e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8648ba133720f9675620",
+    "url": "/Tic-Tac-Toe/static/js/main.e18cb390.chunk.js"
+  },
+  {
+    "revision": "5965f36d7f72120b2bad",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.49965c0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tic-Tac-Toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
